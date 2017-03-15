474d373fe20521d78eb15a260fdf5e0e5077eaed
CAINA COSTA TREVISAN <cct11@inf.ufpr.br>
2017-03-14 22:27:37 -0300

    New main file. Currently reads the JSON files to LUA tables.

--- main.lua
+++ main.lua
@@ -1,93 +1,91 @@
 local class = require 'lupy'
+local json = assert (loadfile "json.lua")()
 
-Battlefield = {}
+Current_Board = {}
 for i=1,6 do
-	Battlefield[i] = {}
+	Current_Board[i] = {}	
 	for j=1,6 do
-		Battlefield[i][j] = nil
+		Current_Board[i][j] = nil
 	end
 end
 
--- Professions:
+Boards = {}
 Professions = {}
+Characters = {}
 
--- Assassin
-Assassin = { ["name"] = "Assassin",
-			 ["atk"] = 5,
-             ["def"] = 5,
-             ["spd"] = 1 }
+--pre-determined professions
+local function read_profs(path)
+	local open = io.open
+	local file = open(path, "r")	
+	if not file then return nil end
 
-Professions["Assassin"] = Assassin
+	profs = file:read "*a"
 
-class [[Character]]
-    function __init__(self, name, troops, actions, atk, def, spd, prof)
-        self.name = name
-        self.troops = troops
-		self.actions = actions
-        self.atk = atk
-        self.def = def
-        self.spd = spd
-		self.prof = prof
-    end
+	file:close()
 
-    function print(self)
-           print (self.name, self.troops, self.actions, self.atk, self.def, self.spd)
-    end
+	Professions = json:decode(profs)
 
-_end()
+--	for i,j in pairs(Professions) do
+--		print("Profession", i)
+--		for m,n in pairs(j) do
+--	    	print (m, n)
+--		end	
+--		print('\n')
+--	end
 
-local A = Character("A", 100, 2, 1, 1, 1, Assassin)
-local B = Character("B", 100, 2, 1, 1, 1, Assassin)
-
-function resolve_atk(atkr, defr)
-
-	-- Resolve Defender damage
-	dmg = math.ceil(( ( atkr["troops"] * (atkr["prof"]["atk"]+atkr["atk"]) ) ^ 1.27 - 
-		  				( defr["troops"] * (defr["prof"]["def"]*1.5+defr["def"])) ^ 1.1 ) *.01 )
-
-	defr["troops"] = defr["troops"] - dmg
-	
-	-- Resolve Attacker damage
-	dmg = math.ceil(( ( defr["troops"] * (defr["prof"]["atk"]+defr["atk"]) ) ^ 1.25 - 
-		  				( atkr["troops"] * (atkr["prof"]["def"]*1.5+atkr["def"])) ^ 1.1 ) *.01 )
-
-	atkr["troops"] = atkr["troops"] - dmg
-
---	attacker["actions"] = attacker["actions"] - 1
 end
 
-local turns = 25
+-- pre-determined characters
+local function read_chars(path)
+	local open = io.open
+	local file = open(path, "r")	
+	if not file then return nil end
 
-A.print()
-B.print()
+	chars = file:read "*a"
 
-resolve_atk(A,B)
+	file:close()
 
-A.print()
-B.print()
+	Characters = json:decode(chars)
 
-
-local open = io.open
-
-local function read_file(path)
-    local file = open(path, "r") 
-    if not file then return nil end
-
-    for i=0,3 do
-		for j=0,3 do
-			Battlefield[i][j] = file:read "*a"
-		end
+	for i,j in pairs(Characters) do
+		print("Character", i)
+		for m,n in pairs(j) do
+	    	print (m, n)
+		end	
+		print('\n')
 	end
 
-    file:close()
 end
 
-local read_pos("atkr");
-print (atkr_pos);
+-- character positions provided by players
+local function read_board(path)
+	local open = io.open
+	local file = open(path, "r")	
+	if not file then return nil end
 
-while turns>0 do
+	boards = file:read "*a"
 
-	
+	file:close()
 
-	turns=turns-1
+	Boards = json:decode(boards)
+
+	for i,j in pairs(Boards) do
+		print("Board", i)
+		for m,n in pairs(j) do
+			for p=1,3 do
+	    		io.write(n[p])
+			end
+			print('\n')
+		end	
+		print('\n')
+	end
+
 end
+
+-- Main
+
+local profs = read_profs(arg[1])
+local chars = read_chars(arg[2])
+local board = read_board(arg[3])
+
+
