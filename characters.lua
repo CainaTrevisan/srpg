609bd0cee820364bdd1829c7d7dc0bbc1a370bd5
CAINA COSTA TREVISAN <cct11@inf.ufpr.br>
2017-03-30 20:27:58 -0300

    added print a single char and a copy function

--- characters.lua
+++ characters.lua
@@ -22,19 +22,49 @@ Character = {
 		prof   = Profession["Defender"]
 
 	},
-	print = function (__self)
-		for k,i in pairs(__self) do
-			if i ~= __self.print then
-				print (k)
-				for l,j in pairs(i) do
-					if j == __self[k].prof then
-						print(l, __self[k].prof.name)
-					else
-						print(l, j)
+	print = function (__self, char)
+
+		if char == nil then	
+
+			-- print all characters in list
+			for k,i in pairs(__self) do
+				if type(i) ~= 'function' then
+					print (k)
+					for l,j in pairs(i) do
+						-- if value references a profession print its name
+						if j == __self[k].prof then
+							print(l, __self[k].prof.name)
+						else
+							print(l, j)
+						end
 					end
-				end
-				io.write("\n")
+					io.write("\n")
 				end	
 			end		
+
+		else
+
+			-- print the character received as argument
+			for k,i in pairs(char) do
+				if k == "prof" then
+					print(k, char.prof.name)
+				else
+					print(k, i)
+				end
+				io.write("\n")
+			end
+
+		end
+
+	end,
+	
+	copy = function(__self, char)
+		new = {}
+		
+		for k,i in pairs(char) do
+			new[k] = i
+		end	
+
+		return new	
 	end
 }
